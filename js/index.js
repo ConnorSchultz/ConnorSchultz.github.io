2e28862cda96633ac20f6857fc66e076acef5fd5
ym985 <yiz637@ucsd.edu>
2017-11-14 16:10:50 -0800

    js

--- js/index.js
+++ js/index.js
@@ -3,9 +3,9 @@ var user;
 window.onload = function(){
     email = localStorage.getItem('currentUser');
     user = JSON.parse(localStorage.getItem(email));
-    calendar = document.getElementById(calendar);
+    calendar = document.getElementById('calendar');
     calendar.rows[0].cells[0].innerHTML = 'aaaa';
-    document.getElementById("squats").innerHTML = '';
-    document.getElementById("yoga").innerHTML = '';
+    document.getElementById('squats').innerHTML = '';
+    document.getElementById('yoga').innerHTML = '';
 
 }
